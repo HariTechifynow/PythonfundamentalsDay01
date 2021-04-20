812ba2549af81cde12034057258d22f3b3814390
HariTechifynow <82362691+HariTechifynow@users.noreply.github.com>
2021-04-20 20:40:15 +0530

    Hari krishna Day01 code
    Hari Krishna Day01 code

---
+++ Day01-B42-Hari Krishna.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Good Morning, Welcome to Python Fundamentals Class.")
+
+
+# In[2]:
+
+
+print(1+2)
+
+
+# In[3]:
+
+
+print(2-3)
+
+
+# In[4]:
+
+
+print(2*3)
+
+
+# In[ ]:
+
+
+
+
